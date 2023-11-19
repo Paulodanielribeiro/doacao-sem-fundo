fa0501737561752140f22db585385770ed06b2e5
Paulo Daniel Moraes Ribeiro <paulomoraesr6@gmail.com>
2023-11-19 09:32:31 -0400

    add
    coloquei mais ou menos, algumas informaçoes em home

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,10 +1,18 @@
-import { Container } from "./styles";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
 
-const Home = () => {
+const Home: React.FC = () => {
+  const nomeOrganizacao = 'Nome da Organização';
+  const tipoDoacao = 'Tipo de Doação';
+  const descricao = 'Descrição/Resumo da Organização';
+
   return (
-    <>
-      <Container />
-    </>
+    <View style={styles.container}>
+      <Text style={styles.header}>{nomeOrganizacao}</Text>
+      <Text style={styles.subtitle}>{tipoDoacao}</Text>
+      <Text style={styles.description}>{descricao}</Text>
+    </View>
   );
 };
 
--- src/pages/homepage/styles.ts
+++ src/pages/homepage/styles.ts
@@ -3,4 +3,7 @@ import { View } from "react-native";
 
 export const Container = styled.View`
   background-color: red;
+  margin: 0;
+  widthe: 100px;
+  
 `;
